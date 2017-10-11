b42b9d8b50e0533fc781c9c63a9e28f69262dc43
Mahadi Hasan <31430772+codermahadi@users.noreply.github.com>
2017-10-12 00:47:17 +0600

    view resolver 2
    View resolver updated version using Model class

--- Spring4WebMVN/src/main/java/com/mahadi/controller/HomeController.java
+++ Spring4WebMVN/src/main/java/com/mahadi/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.mahadi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 /**
  * Created by Mahadi on 10/10/2017.
@@ -10,7 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = "/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("name", "Hasan");
         return "home";
     }
+
+//    @RequestMapping(value = "/")
+//    public ModelAndView home(){
+//        ModelAndView modelAndView = new ModelAndView("home");
+//        Map<String, Object> model = modelAndView.getModel();
+//        model.put("name", "Mahadi");
+//        return modelAndView;
+//    }
 }
--- Spring4WebMVN/src/main/webapp/WEB-INF/jsps/home.jsp
+++ Spring4WebMVN/src/main/webapp/WEB-INF/jsps/home.jsp
@@ -6,11 +6,17 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home | 2017 </title>
 </head>
 <body>
-Welcome To Spring Web MVC
+Welcome To Spring Web MVC <br>
+Request: ${name}
 </body>
 </html>
